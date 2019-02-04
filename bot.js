20936f9d22065f64959927553d212ee48d34f687
hey <44032576+YASSIN974@users.noreply.github.com>
2019-02-04 11:34:36 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ client.on('message', message =>{
     let args = message.content.split(' ');
     let prefix = 'R'; //تقدر تغير البرفكس
     
-    if(args[0] === `${prefix}a`){
+    if(args[0] === `${prefix}avatar`){
         let mentions = message.mentions.members.first()
         if(!mentions) {
           let sicon = message.author.avatarURL
